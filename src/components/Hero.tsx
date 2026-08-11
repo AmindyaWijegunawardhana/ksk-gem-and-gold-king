e35fb427015a40c66eeb949d631aa90657a63cdd
NC MOBILE <amindyanw911@gmail.com>
2026-08-11 23:27:14 +0530

    Add padding and containment to Hero logo to prevent clipping

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ export const Hero: React.FC = () => {
             <img
               src={logo}
               alt="KSK Gem and Gold King Logo"
-              className="w-80 h-80 md:w-96 md:h-96 lg:w-[440px] lg:h-[440px] object-cover rounded-full shadow-lg border-4 border-[#C9A227]/30 hover:scale-[1.02] transition-transform duration-500"
+              className="w-80 h-80 md:w-96 md:h-96 lg:w-[440px] lg:h-[440px] object-contain p-6 bg-black rounded-full shadow-lg border-4 border-[#C9A227]/30 hover:scale-[1.02] transition-transform duration-500"
             />
           </div>
 
